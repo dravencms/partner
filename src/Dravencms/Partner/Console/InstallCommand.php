df419846504104851b656d1d3697498aac6be9a0
Adam Schubert <adam.schubert@sg1-game.net>
2016-11-24 02:44:06 +0100

    Bugfix

--- src/Dravencms/Partner/Console/InstallCommand.php
+++ src/Dravencms/Partner/Console/InstallCommand.php
@@ -37,12 +37,12 @@ class InstallCommand extends Command
 
             $entityManager->persist($aclResource);
 
-            $aclOperation = new AclOperation($aclResource, 'edit', 'Allows editation of partner');
-            $entityManager->persist($aclOperation);
-            $aclOperation = new AclOperation($aclResource, 'delete', 'Allows deletion of partner');
-            $entityManager->persist($aclOperation);
+            $aclOperationEdit = new AclOperation($aclResource, 'edit', 'Allows editation of partner');
+            $entityManager->persist($aclOperationEdit);
+            $aclOperationDelete = new AclOperation($aclResource, 'delete', 'Allows deletion of partner');
+            $entityManager->persist($aclOperationDelete);
 
-            $adminMenu = new Menu('Partner', ':Admin:Partner:Partner', 'fa-diamond', $aclOperation);
+            $adminMenu = new Menu('Partner', ':Admin:Partner:Partner', 'fa-diamond', $aclOperationEdit);
 
             $foundRoot = $adminMenuRepository->getOneByName('Site items');
 
